02c3ebf0d3e826a99eb975303e5672129374a708
Bartosz Król <barto.kro@gmail.com>
2021-05-17 01:31:45 +0200

    set basic css styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
 		name: "",
 		lastName: "",
 		dateOfBirth: "",
+		sex: "",
+		maritalStatus: "",
 		country: "",
 		city: "",
 		street: "",
@@ -16,51 +18,130 @@ class App extends Component {
 		return (
 			<div className="form">
 				<form className="form__firstPage" noValidate>
-					<label htmlFor="name">Name:</label>
-					<input type="text" id="name" value={this.state.name} />
-					<span>Message</span>
-					<label htmlFor="lastname">Last Name: </label>
+					<label
+						htmlFor="name"
+						className="form__firstPage__labelName"
+					>
+						Name:
+					</label>
+					<input type="text" id="name" />
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="lastname"
+						className="form__firstPage__labelName"
+					>
+						Last Name:{" "}
+					</label>
 					<input
 						type="text"
 						id="lastname"
 						value={this.state.lastName}
 					/>
-					<span>Message</span>
-					<label htmlFor="dateOfBirth">Date of Birth: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="dateOfBirth"
+						className="form__firstPage__labelName"
+					>
+						Date of Birth:{" "}
+					</label>
 					<input
 						type="text"
 						id="dateOfBirth"
 						value={this.state.dateOfBirth}
 					/>
-					<span>Message</span>
-					<label htmlFor="country">Country: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label htmlFor="sex" className="form__firstPage__labelName">
+						Sex:{" "}
+					</label>
+					<input type="text" id="sex" value={this.state.sex} />
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="maritalStatus"
+						className="form__firstPage__labelName"
+					>
+						Marital Status:{" "}
+					</label>
+					<input
+						type="text"
+						id="maritalStatus"
+						value={this.state.maritalStatus}
+					/>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					{/* <label
+						htmlFor="country"
+						className="form__firstPage__labelName"
+					>
+						Country:{" "}
+					</label>
 					<input
 						type="text"
 						id="country"
 						value={this.state.country}
 					/>
-					<span>Message</span>
-					<label htmlFor="city">City: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="city"
+						className="form__firstPage__labelName"
+					>
+						City:{" "}
+					</label>
 					<input type="text" id="city" value={this.state.city} />
-					<span>Message</span>
-					<label htmlFor="street">Street: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="street"
+						className="form__firstPage__labelName"
+					>
+						Street:{" "}
+					</label>
 					<input type="text" id="street" value={this.state.street} />
-					<span>Message</span>
-					<label htmlFor="building_number">Building Number: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="building_number"
+						className="form__firstPage__labelName"
+					>
+						Building Number:{" "}
+					</label>
 					<input
 						type="text"
 						id="building_number"
 						value={this.state.buildingNumber}
 					/>
-					<span>Message</span>
-					<label htmlFor="postal_code">Postal Code: </label>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span>
+					<label
+						htmlFor="postal_code"
+						className="form__firstPage__labelName"
+					>
+						Postal Code:{" "}
+					</label>
 					<input
 						type="text"
 						id="postal_code"
 						value={this.state.postalCode}
 					/>
-					<span>Message</span>
-					<button type="submit">Wyślij</button>
+					<span className="form__firstPage__errorMessage">
+						Error message
+					</span> */}
+					<button type="submit" class="form__button">
+						Next Page
+					</button>
 				</form>
 			</div>
 		);
