ff86d44d8d22a7cb54c7e9d4a1e4f351be763d95
Bartosz Król <barto.kro@gmail.com>
2021-05-24 00:57:12 +0200

    change structure

--- src/App.js
+++ src/App.js
@@ -1,84 +1,27 @@
 import { Component } from "react";
-import Input from "./Input";
+import FirstPage from "./FirstPage";
 import "./styles/App.scss";
 
 class App extends Component {
 	state = {
-		name: {
-			id: "name",
-			label: "Name",
-			value: "",
-			input: {
-				input: "input",
-				type: "text",
-			},
-			error: false,
+		name: "",
+		lastName: "",
+		dateOfBirth: "",
+		sex: "",
+		maritalStatus: "",
+		firstPageErrors: {
+			name_err: false,
+			lastName_err: false,
+			dateOfBirth_err: false,
+			sex_err: false,
+			maritalStatus_err: false,
 		},
-		lastName: {
-			id: "lastName",
-			label: "Last Name",
-			value: "",
-			input: {
-				input: "input",
-				type: "text",
-			},
-			error: false,
-		},
-		dateOfBirth: {
-			id: "dateOfBirth",
-			label: "Date of Birth",
-			value: "",
-			input: {
-				input: "input",
-				type: "date",
-			},
-			error: false,
-		},
-		sex: {
-			id: "sex",
-			label: "Sex",
-			value: "",
-			input: {
-				input: "select",
-				type: "text",
-				options: ["", "Male", "Female", "Other"],
-			},
-			error: false,
-		},
-		maritalStatus: {
-			id: "maritalStatus",
-			label: "Marital Status",
-			value: "",
-			input: {
-				input: "select",
-				type: "text",
-				options: [
-					"",
-					"Single",
-					"Married",
-					"Separated",
-					"Divorced",
-					"Widowed",
-					"Other",
-				],
-			},
-			error: false,
-		},
+		firstPageValidated: false,
 		country: "",
 		city: "",
 		street: "",
 		buildingNumber: "",
 		postalCode: "",
-		// errors: {
-		// 	name_err: false,
-		// 	lastName_err: false,
-		// 	dateOfBirth_err: false,
-		// 	sex_err: false,
-		// 	maritalStatus_err: false,
-		// },
-		pagesValidated: {
-			firstPage: false,
-		},
 	};
 
 	errorMessageActive = "form__firstPage__errorMessage active";
@@ -86,54 +29,9 @@ class App extends Component {
 
 	handleInputChange = (e) => {
 		const inputName = e.target.id;
-		const labelName = e.target.previousSibling.textContent;
-		const type = e.target.type;
-		if (e.target.localName === "input" && type === "text") {
-			this.setState({
-				[inputName]: {
-					id: inputName,
-					label: labelName,
-					value: e.target.value,
-					input: {
-						input: "input",
-						type: "text",
-					},
-				},
-			});
-		}
-
-		if (e.target.localName === "input" && type === "date") {
-			this.setState({
-				[inputName]: {
-					id: inputName,
-					label: labelName,
-					value: e.target.value,
-					input: {
-						input: "input",
-						type: "date",
-					},
-				},
-			});
-		}
-
-		if (e.target.localName === "select") {
-			const options = [];
-			Array.from(e.target.options).forEach((option) =>
-				options.push(option.value)
-			);
-			this.setState({
-				[inputName]: {
-					id: inputName,
-					label: labelName,
-					value: e.target.value,
-					input: {
-						input: "select",
-						type: "text",
-						options: options,
-					},
-				},
-			});
-		}
+		this.setState({
+			[inputName]: e.target.value,
+		});
 	};
 
 	handleSubmitFirstPage = (e) => {
@@ -148,19 +46,14 @@ class App extends Component {
 			isPageValidated,
 		} = validated;
 		this.setState({
-			// errors: {
-			// 	name_err: !name,
-			// 	lastName_err: !lastName,
-			// 	dateOfBirth_err: !dateOfBirth,
-			// 	sex_err: !sex,
-			// 	maritalStatus_err: !maritalStatus,
-			// },
-			name: {
-				error: !name,
+			firstPageErrors: {
+				name_err: !name,
+				lastName_err: !lastName,
+				dateOfBirth_err: !dateOfBirth,
+				sex_err: !sex,
+				maritalStatus_err: !maritalStatus,
 			},
-			pagesValidated: {
-				firstPage: isPageValidated,
-			},
+			firstPageValidated: isPageValidated,
 		});
 	};
 	checkForm() {
@@ -174,24 +67,24 @@ class App extends Component {
 		const date = this.checkDateOfBirthValidation(dateOfBirth);
 		dateOfBirth = date;
 
-		if (this.state.name.value.length > 3) {
+		if (this.state.name.length > 3) {
 		} else {
 			name = false;
 		}
 
-		if (this.state.lastName.value.length > 3) {
+		if (this.state.lastName.length > 3) {
 		} else {
 			lastName = false;
 		}
-		if (dateOfBirth.value) {
+		if (dateOfBirth) {
 		} else {
 			dateOfBirth = false;
 		}
-		if (this.state.sex.value) {
+		if (this.state.sex) {
 		} else {
 			sex = false;
 		}
-		if (this.state.maritalStatus.value) {
+		if (this.state.maritalStatus) {
 		} else {
 			maritalStatus = false;
 		}
@@ -211,9 +104,9 @@ class App extends Component {
 	}
 
 	checkDateOfBirthValidation(dateOfBirth) {
-		const year = Number(this.state.dateOfBirth.value.slice(0, 4));
-		const month = Number(this.state.dateOfBirth.value.slice(5, 7));
-		const day = Number(this.state.dateOfBirth.value.slice(8, 10));
+		const year = Number(this.state.dateOfBirth.slice(0, 4));
+		const month = Number(this.state.dateOfBirth.slice(5, 7));
+		const day = Number(this.state.dateOfBirth.slice(8, 10));
 
 		const date = new Date();
 		const todaysDay = date.getDate();
@@ -252,178 +145,23 @@ class App extends Component {
 	}
 
 	render() {
-		// const { name, lastName, dateOfBirth, sex, maritalStatus } = this.state;
-		// const {
-		// 	name_err,
-		// 	lastName_err,
-		// 	dateOfBirth_err,
-		// 	sex_err,
-		// 	maritalStatus_err,
-		// } = this.state.errors;
-
 		return (
 			<div className="form">
 				<form className="form__firstPage" noValidate>
-					<Input
-						id={this.state.name}
-						name={this.state.name}
-						click={this.handleInputChange}
-						activeMessage={this.errorMessageActive}
-						hiddenMessage={this.errorMessageHidden}
-					/>
-					<Input
-						id={this.state.lastName}
-						name={this.state.lastName}
-						click={this.handleInputChange}
-						activeMessage={this.errorMessageActive}
-						hiddenMessage={this.errorMessageHidden}
-					/>
-					<Input
-						id={this.state.dateOfBirth}
-						name={this.state.dateOfBirth}
-						click={this.handleInputChange}
-						activeMessage={this.errorMessageActive}
-						hiddenMessage={this.errorMessageHidden}
-					/>
-					<Input
-						id={this.state.sex}
-						name={this.state.sex}
-						click={this.handleInputChange}
-						activeMessage={this.errorMessageActive}
-						hiddenMessage={this.errorMessageHidden}
-					/>
-					<Input
-						id={this.state.maritalStatus}
-						name={this.state.maritalStatus}
-						click={this.handleInputChange}
-						activeMessage={this.errorMessageActive}
-						hiddenMessage={this.errorMessageHidden}
-					/>
-
-					{/* <label
-						htmlFor="name"
-						className="form__firstPage__labelName"
-					>
-						Name:
-					</label>
-					<input
-						type="text"
-						id="name"
-						autoComplete="off"
-						value={name.value}
-						onChange={this.handleInputChange}
-					/>
-					<span
-						className={
-							name_err
-								? this.errorMessageActive
-								: this.errorMessageHidden
-						}
-					>
-						Error message
-					</span>
-					<label
-						htmlFor="lastName"
-						className="form__firstPage__labelName"
-					>
-						Last Name:{" "}
-					</label>
-					<input
-						type="text"
-						id="lastName"
-						value={lastName.value}
-						autoComplete="off"
-						onChange={this.handleInputChange}
-					/>
-					<span
-						className={
-							lastName_err
-								? this.errorMessageActive
-								: this.errorMessageHidden
-						}
-					>
-						Error message
-					</span>
-					<label
-						htmlFor="dateOfBirth"
-						className="form__firstPage__labelName"
-					>
-						Date of Birth:{" "}
-					</label>
-					<input
-						type="date"
-						id="dateOfBirth"
-						value={dateOfBirth.value}
-						autoComplete="off"
-						onChange={this.handleInputChange}
-					/>
-					<span
-						className={
-							dateOfBirth_err
-								? this.errorMessageActive
-								: this.errorMessageHidden
-						}
-					>
-						Error message
-					</span>
-					<label htmlFor="sex" className="form__firstPage__labelName">
-						Sex:{" "}
-					</label>
-					<select
-						id="sex"
-						name="sex"
-						onChange={this.handleInputChange}
-						value={sex.value}
-					>
-						<option value=""></option>
-						<option value="male">Male</option>
-						<option value="female">Female</option>
-						<option value="other">Other</option>
-					</select>
-
-					<span
-						className={
-							sex_err
-								? this.errorMessageActive
-								: this.errorMessageHidden
-						}
-					>
-						Error message
-					</span>
-					<label
-						htmlFor="maritalStatus"
-						className="form__firstPage__labelName"
-					>
-						Marital Status:{" "}
-					</label>
-					<select
-						id="maritalStatus"
-						name="maritalStatus"
-						onChange={this.handleInputChange}
-						value={maritalStatus.value}
-					>
-						<option value=""></option>
-						<option value="single">Single</option>
-						<option value="married">Married</option>
-						<option value="separated">Separated</option>
-						<option value="divorced">Divorced</option>
-						<option value="widowed">Widowed</option>
-					</select>
-					<span
-						className={
-							maritalStatus_err
-								? this.errorMessageActive
-								: this.errorMessageHidden
-						}
-					>
-						Error message
-					</span> */}
-					{/* <button
-						class="form__button"
-						onClick={this.handleSubmitFirstPage}
-					>
-						Next Page
-					</button> */}
+					{!this.state.firstPageValidated ? (
+						<FirstPage
+							name={this.state.name}
+							lastName={this.state.lastName}
+							dateOfBirth={this.state.dateOfBirth}
+							sex={this.state.sex}
+							maritalStatus={this.state.maritalStatus}
+							errors={this.state.firstPageErrors}
+							click={this.handleInputChange}
+							submit={this.handleSubmitFirstPage}
+							messageActive={this.errorMessageActive}
+							messageHidden={this.errorMessageHidden}
+						/>
+					) : null}
 					{/* <label
 						htmlFor="country"
 						className="form__firstPage__labelName"
---
+++ src/FirstPage.js
@@ -0,0 +1,117 @@
+const FirstPage = (props) => {
+	const { name, lastName, dateOfBirth, sex, maritalStatus } = props;
+	const {
+		name_err,
+		lastName_err,
+		dateOfBirth_err,
+		sex_err,
+		maritalStatus_err,
+	} = props.errors;
+
+	return (
+		<>
+			<label htmlFor="name" className="form__firstPage__labelName">
+				Name:
+			</label>
+			<input
+				type="text"
+				id="name"
+				autoComplete="off"
+				value={name.value}
+				onChange={props.click}
+			/>
+			<span
+				className={name_err ? props.messageActive : props.messageHidden}
+			>
+				Error message
+			</span>
+			<label htmlFor="lastName" className="form__firstPage__labelName">
+				Last Name:{" "}
+			</label>
+			<input
+				type="text"
+				id="lastName"
+				value={lastName.value}
+				autoComplete="off"
+				onChange={props.click}
+			/>
+			<span
+				className={
+					lastName_err ? props.messageActive : props.messageHidden
+				}
+			>
+				Error message
+			</span>
+			<label htmlFor="dateOfBirth" className="form__firstPage__labelName">
+				Date of Birth:{" "}
+			</label>
+			<input
+				type="date"
+				id="dateOfBirth"
+				value={dateOfBirth.value}
+				autoComplete="off"
+				onChange={props.click}
+			/>
+			<span
+				className={
+					dateOfBirth_err ? props.messageActive : props.messageHidden
+				}
+			>
+				Error message
+			</span>
+			<label htmlFor="sex" className="form__firstPage__labelName">
+				Sex:{" "}
+			</label>
+			<select
+				id="sex"
+				name="sex"
+				onChange={props.click}
+				value={sex.value}
+			>
+				<option value=""></option>
+				<option value="male">Male</option>
+				<option value="female">Female</option>
+				<option value="other">Other</option>
+			</select>
+
+			<span
+				className={sex_err ? props.messageActive : props.messageHidden}
+			>
+				Error message
+			</span>
+			<label
+				htmlFor="maritalStatus"
+				className="form__firstPage__labelName"
+			>
+				Marital Status:{" "}
+			</label>
+			<select
+				id="maritalStatus"
+				name="maritalStatus"
+				onChange={props.click}
+				value={maritalStatus.value}
+			>
+				<option value=""></option>
+				<option value="single">Single</option>
+				<option value="married">Married</option>
+				<option value="separated">Separated</option>
+				<option value="divorced">Divorced</option>
+				<option value="widowed">Widowed</option>
+			</select>
+			<span
+				className={
+					maritalStatus_err
+						? props.messageActive
+						: props.messageHidden
+				}
+			>
+				Error message
+			</span>
+			<button class="form__button" onClick={props.submit}>
+				Next Page
+			</button>
+		</>
+	);
+};
+
+export default FirstPage;
