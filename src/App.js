ce753c73441f81f7b77c3ff65c60c2fa24c33f71
Bartosz Król <barto.kro@gmail.com>
2021-05-30 23:22:04 +0200

    remove unneeded code in comments

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import "./styles/App.scss";
 
 class App extends Component {
 	state = {
-		// name: "",
-		// lastName: "",
-		// dateOfBirth: "",
-		// sex: "",
-		// maritalStatus: "",
 		firstPageInputs: [
 			{
 				id: 0,
@@ -147,37 +142,6 @@ class App extends Component {
 		e.preventDefault();
 
 		this.checkForm(this.state.firstPageInputs);
-		// const {
-		// 	name,
-		// 	lastName,
-		// 	dateOfBirth,
-		// 	sex,
-		// 	maritalStatus,
-		// 	name_message,
-		// 	lastName_message,
-		// 	dateOfBirth_message,
-		// 	sex_message,
-		// 	maritalStatus_message,
-		// 	isPageValidated,
-		// } = this.checkFirstPage();
-
-		// this.setState(() => ({
-		// 	firstPageErrors: {
-		// 		name_err: !name,
-		// 		lastName_err: !lastName,
-		// 		dateOfBirth_err: !dateOfBirth,
-		// 		sex_err: !sex,
-		// 		maritalStatus_err: !maritalStatus,
-		// 	},
-		// 	firstPageErrorsMessages: {
-		// 		name_message: name_message,
-		// 		lastName_message: lastName_message,
-		// 		dateOfBirth_message: dateOfBirth_message,
-		// 		sex_message: sex_message,
-		// 		maritalStatus_message: maritalStatus_message,
-		// 	},
-		// 	firstPageValidated: isPageValidated,
-		// }));
 	};
 
 	checkForm(pageInputs) {
@@ -228,86 +192,6 @@ class App extends Component {
 			firstPageInputs: inputs,
 		});
 	}
-
-	// checkFirstPage() {
-	// 	let name = true;
-	// 	let lastName = true;
-	// 	let dateOfBirth = true;
-	// 	let sex = true;
-	// 	let maritalStatus = true;
-	// 	let isPageValidated = false;
-
-	// 	let name_message = " ";
-	// 	let lastName_message = " ";
-	// 	let dateOfBirth_message = " ";
-	// 	let sex_message = " ";
-	// 	let maritalStatus_message = " ";
-
-	// 	const checkName = this.checkTextInput(
-	// 		this.state.name,
-	// 		name,
-	// 		name_message
-	// 	);
-	// 	name = checkName.stateName;
-	// 	name_message = checkName.stateName_message;
-
-	// 	const checkLastName = this.checkTextInput(
-	// 		this.state.lastName,
-	// 		lastName,
-	// 		lastName_message
-	// 	);
-	// 	lastName = checkLastName.stateName;
-	// 	lastName_message = checkLastName.stateName_message;
-
-	// const date = this.checkDateOfBirthValidation(dateOfBirth);
-	// dateOfBirth = date;
-
-	// 	const checkDateOfBirth = this.checkDateOfBirth(
-	// 		dateOfBirth,
-	// 		dateOfBirth_message
-	// 	);
-	// 	dateOfBirth = checkDateOfBirth.dateOfBirth;
-	// 	dateOfBirth_message = checkDateOfBirth.dateOfBirth_message;
-
-	// 	const checkSex = this.checkSelectInput(
-	// 		this.state.sex,
-	// 		sex,
-	// 		sex_message
-	// 	);
-	// 	sex = checkSex.stateName;
-	// 	sex_message = checkSex.stateName_message;
-
-	// 	const checkMaritalStatus = this.checkSelectInput(
-	// 		this.state.maritalStatus,
-	// 		maritalStatus,
-	// 		maritalStatus_message
-	// 	);
-	// 	maritalStatus = checkMaritalStatus.stateName;
-	// 	maritalStatus_message = checkMaritalStatus.stateName_message;
-
-	// 	if (name && lastName && dateOfBirth && sex && maritalStatus) {
-	// 		isPageValidated = true;
-	// 		this.setState({
-	// 			firstPageValidated: isPageValidated,
-	// 			firstPageVisible: false,
-	// 			secondPageVisible: true,
-	// 		});
-	// 	}
-
-	// 	return {
-	// 		name,
-	// 		lastName,
-	// 		dateOfBirth,
-	// 		sex,
-	// 		maritalStatus,
-	// 		name_message,
-	// 		lastName_message,
-	// 		dateOfBirth_message,
-	// 		sex_message,
-	// 		maritalStatus_message,
-	// 		isPageValidated,
-	// 	};
-	// }
 
 	checkTextInput(value, error, errorMessage) {
 		if (!value.match(this.regex.lettersOnly)) {
@@ -379,130 +263,6 @@ class App extends Component {
 		}
 	}
 
-	// handlePageChange = (e) => {
-	// 	e.preventDefault();
-	// 	if (this.state.secondPageVisible) {
-	// 		this.setState({
-	// 			firstPageValidated: false,
-	// 			firstPageVisible: true,
-	// 			secondPageVisible: false,
-	// 		});
-	// 	}
-	// };
-
-	// checkSecondPage() {
-	// 	let country = true;
-	// 	let city = true;
-	// 	let street = true;
-	// 	let buildingNumber = true;
-	// 	let postalCode = true;
-	// 	let isPageValidated = false;
-
-	// 	let country_message = " ";
-	// 	let city_message = " ";
-	// 	let street_message = " ";
-	// 	let buildingNumber_message = " ";
-	// 	let postalCode_message = " ";
-
-	// 	const checkCountry = this.checkTextInput(
-	// 		this.state.country,
-	// 		country,
-	// 		country_message
-	// 	);
-	// 	country = checkCountry.stateName;
-	// 	country_message = checkCountry.stateName_message;
-
-	// 	const checkCity = this.checkTextInput(
-	// 		this.state.city,
-	// 		city,
-	// 		city_message
-	// 	);
-	// 	city = checkCity.stateName;
-	// 	city_message = checkCity.stateName_message;
-
-	// 	const checkStreet = this.checkTextInput(
-	// 		this.state.street,
-	// 		street,
-	// 		street_message
-	// 	);
-	// 	street = checkStreet.stateName;
-	// 	street_message = checkStreet.stateName_message;
-
-	// 	const checkBuildingNumber = this.checkTextInput(
-	// 		this.state.buildingNumber,
-	// 		buildingNumber,
-	// 		buildingNumber_message
-	// 	);
-	// 	buildingNumber = checkBuildingNumber.stateName;
-	// 	buildingNumber_message = checkBuildingNumber.stateName_message;
-
-	// 	const checkPostalCode = this.checkTextInput(
-	// 		this.state.maritalStatus,
-	// 		postalCode,
-	// 		postalCode_message
-	// 	);
-	// 	postalCode = checkPostalCode.stateName;
-	// 	postalCode_message = checkPostalCode.stateName_message;
-
-	// 	if (country && city && street && buildingNumber && postalCode) {
-	// 		isPageValidated = true;
-	// 		this.setState({
-	// 			firstPageValidated: isPageValidated,
-	// 			firstPageVisible: false,
-	// 			secondPageVisible: true,
-	// 		});
-	// 	}
-
-	// 	return {
-	// 		country,
-	// 		city,
-	// 		street,
-	// 		buildingNumber,
-	// 		postalCode,
-	// 		country_message,
-	// 		city_message,
-	// 		street_message,
-	// 		buildingNumber_message,
-	// 		postalCode_message,
-	// 		isPageValidated,
-	// 	};
-	// }
-
-	// handleSubmitSecondPage = (e) => {
-	// 	e.preventDefault();
-	// 	const {
-	// 		country,
-	// 		city,
-	// 		street,
-	// 		buildingNumber,
-	// 		postalCode,
-	// 		country_message,
-	// 		city_message,
-	// 		street_message,
-	// 		buildingNumber_message,
-	// 		postalCode_message,
-	// 		isPageValidated,
-	// 	} = this.checkSecondPage();
-
-	// 	this.setState(() => ({
-	// 		secondPageErrors: {
-	// 			country_err: !country,
-	// 			city_err: !city,
-	// 			street_err: !street,
-	// 			buildingNumber_err: !buildingNumber,
-	// 			postalCode_err: !postalCode,
-	// 		},
-	// 		secondPageErrorsMessages: {
-	// 			country_message: country_message,
-	// 			city_message: city_message,
-	// 			street_message: street_message,
-	// 			buildingNumber_message: buildingNumber_message,
-	// 			postalCode_message: postalCode_message,
-	// 		},
-	// 		secondPageValidated: isPageValidated,
-	// 	}));
-	// };
-
 	render() {
 		return (
 			<div className="form">
@@ -514,15 +274,6 @@ class App extends Component {
 							messageActive={this.errorMessageActive}
 							change={this.handleInputChange}
 							submit={this.handleSubmitFirstPage}
-							// name={this.state.name}
-							// lastName={this.state.lastName}
-							// dateOfBirth={this.state.dateOfBirth}
-							// sex={this.state.sex}
-							// maritalStatus={this.state.maritalStatus}
-							// errors={this.state.firstPageErrors}
-							// messageActive={this.errorMessageActive}
-							// messageHidden={this.errorMessageHidden}
-							// errorMessages={this.state.firstPageErrorsMessages}
 						/>
 					) : null}
 					{/* {this.state.secondPageVisible ? (
